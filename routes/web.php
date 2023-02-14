71edbe8b1e6f65c6b954be58e42619e8fd2b9533
mosta <modtasaad2000@gmail.com>
2023-02-14 08:37:16 +0200

    api-githup

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,17 @@ Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show')
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+use Laravel\Socialite\Facades\Socialite;
+ 
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+ 
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('github')->user();
+ 
+    // $user->token
+});
